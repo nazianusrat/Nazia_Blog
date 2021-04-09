082b576081c2035070f43689ff7dd0d1fb7d58d1
nazianusrat <nazeeyanusratima@gmail.com>
2021-04-09 16:31:44 +0100

    Changed region

--- django_project/settings.py
+++ django_project/settings.py
@@ -153,6 +153,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-
+# AWS_S3_REGION_NAME = "US Standard"
+AWS_S3_REGION_NAME = 'us-east-2' #change to your region
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 django_heroku.settings(locals())
