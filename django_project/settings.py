1b4990dc4c176aaae9b3982eb01e0267c72b2bc2
nazianusrat <nazeeyanusratima@gmail.com>
2021-04-09 15:32:59 +0100

    Updated AWS info

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+# import boto3
 import django_heroku
 # import json
 
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -146,4 +148,11 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 # EMAIL_HOST_USER = config.get('EMAIL_USER')
 # EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 django_heroku.settings(locals())
