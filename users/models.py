017f606a9fc6110b514d4944eb508b8aed770e8d
nazianusrat <nazeeyanusratima@gmail.com>
2021-04-09 15:35:43 +0100

    updated user save model

--- users/models.py
+++ users/models.py
@@ -11,15 +11,15 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 # Resizing the uploaded/saved image
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
 
 # Create your models here.
