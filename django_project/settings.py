ec282d2fa3d8114dac6e48af32f35187b6798699
nazianusrat <nazeeyanusratima@gmail.com>
2021-04-09 11:51:47 +0100

    allow host configured

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-# ALLOWED_HOSTS = ['naziaawsomeblog.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['naziaawsomeblog.herokuapp.com']
+# ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
